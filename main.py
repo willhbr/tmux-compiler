242216dddf325601c744f7903c014c9bd6763199
Will Richardson <william.hamish@gmail.com>
2024-03-08 12:40:40 +0000

    Actually compile a program

--- main.py
+++ main.py
@@ -11,15 +11,23 @@ class Program:
   def next_window_index(self):
     self.index += 1
     return self.index
-  def write(self, contents):
-    self.buffer.append(textwrap.dedent(contents))
+  def write(self, contents, index=None):
+    if contents is None:
+      self.buffer.append(None)
+    elif index is None:
+      self.buffer.append(textwrap.dedent(contents))
+    elif self.buffer[index] is not None:
+      raise Exception("Invalid instruction overwrite")
+    else:
+      self.buffer[index] = textwrap.dedent(contents) 
+    return len(self.buffer) - 1
   def to_string(self):
     return '\n'.join(self.buffer)
 
   def add_header(self):
     self.write("""\
         new-session -d -n %s 'read && tmux wait-for -S %s'
-        set -g display-time 0
+        set -g display-time 2000
         set -g focus-events on
         """ % (self.session, self.session))
 
@@ -45,16 +53,17 @@ class Program:
       """ % (self.session, index, index + 1 if out_index is None else out_index))
 
   def maths_op(self, op):
+    self.store_top()
     index = self.next_window_index()
     self.write("""\
       new-window 'tmux wait-for %s'
       set-hook -t :=%s pane-focus-in {
-        run 'tmux rename-window -t :=4 "#{e|%s:#{buffer_sample},#{window_name}}"'
+        run 'tmux rename-window -t :=%s "#{e|%s:#{buffer_sample},#{window_name}}"'
         run 'tmux delete-buffer'
         run 'tmux set-buffer "#{window_name}"'
         run 'tmux next-window'
       }
-      """ % (self.session, index, op))
+      """ % (self.session, index, index, op))
 
   def print(self):
     index = self.next_window_index()
@@ -67,31 +76,86 @@ class Program:
       }
       """ % (self.session, index))
 
-  def ifeq(self):
-    pass
-   #new-window 'tmux wait-for test-session'
-   #set-hook -t :=4 pane-focus-in {
-   #  run 'tmux select-window -t "#{?#{e|==:#{buffer_sample},#{window_name}},:=5,:=6}"'
-   #  run 'tmux delete-buffer'
-   #}
+  def print_str(self, text):
+    index = self.next_window_index()
+    self.write("""\
+      new-window 'tmux wait-for %s'
+      set-hook -t :=%s pane-focus-in {
+        run 'tmux display -F "%s"'
+        run 'sleep 2'
+        run 'tmux next-window'
+      }
+      """ % (self.session, index, text))
+
+  def cond(self):
+    instruction_index = self.next_window_index()
+    pos = self.write(None)
+    def add_with_indexes_later():
+      self.write("""\
+        new-window 'tmux wait-for %s'
+        set-hook -t :=%s pane-focus-in {
+          run 'tmux select-window -t "#{?#{buffer_sample},:=%s,:=%s}"'
+          run 'tmux delete-buffer'
+        }
+        """ % (self.session, instruction_index, instruction_index + 1, self.index + 1), pos)
+    return add_with_indexes_later
+
+ast.Add.compile_to = lambda self, program: program.maths_op('+')
+ast.Sub.compile_to = lambda self, program: program.maths_op('-')
+ast.Mult.compile_to = lambda self, program: program.maths_op('*')
+ast.Div.compile_to = lambda self, program: program.maths_op('/')
+
+def compile_const(self, program):
+  program.push_constant(self.value)
+ast.Constant.compile_to = compile_const
+
+def compile_binop(self, program):
+  self.left.compile_to(program)
+  self.right.compile_to(program)
+  self.op.compile_to(program)
+ast.BinOp.compile_to = compile_binop
+
+def compile_call(self, program):
+  for arg in self.args:
+    arg.compile_to(program)
+  if self.func.id == 'print':
+    p.print()
+  else:
+    raise Exception("only print is supported")
+ast.Call.compile_to = compile_call
+
+def compile_expr(self, program):
+  self.value.compile_to(program)
+ast.Expr.compile_to = compile_expr
+
+def compile_module(self, program):
+  program.add_header()
+  for expr in self.body:
+    expr.compile_to(program)
+  p.write('select-window -t :=0')
+
+ast.Module.compile_to = compile_module
 
 
 p = Program()
-p.add_header()
-p.push_constant(1)
-p.push_constant(2)
-p.store_top()
-p.maths_op('+')
-p.print()
-p.write('select-window -t :=0')
-
-print(p.to_string())
+# p.push_constant(1)
+# cb = p.cond()
+# p.print_str("Hello world")
+# # p.print_str("second print")
+# p.write('new-window')
+# cb()
+# p.write('select-window -t :=0')
+# 
+# print(p.to_string())
 
 
 with open(sys.argv[1]) as f:
   tree = ast.parse(f.read())
 
 print(ast.dump(tree, indent=2))
+
+tree.compile_to(p)
+print(p.to_string())
 
 with open(sys.argv[2], 'wt') as o:
   o.write(p.to_string())
